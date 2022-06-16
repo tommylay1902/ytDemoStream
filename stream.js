b0c86cfdfe9e8342fe62376285db45b8618acf30
tommylay1902 <tommylay.c@gmail.com>
2022-06-15 20:03:19 -0700

    changed base url

--- stream.js
+++ stream.js
@@ -2,7 +2,7 @@ const passcode = "1234";
 function verifyPasscode() {
   const p = document.getElementById("passcode").value;
   if (p === passcode) {
-    window.location.href = "https://tommylay1902.github.io//hidden.html";
+    window.location.href = "https://tommylay1902.github.io/hidden.html";
   } else {
     alert("wrong!");
   }
